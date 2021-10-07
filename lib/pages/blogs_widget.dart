b4e14bf616c5a68cb8847986cc3dd6251ac2115d
SiferAdmin <59440532+SiferAdmin@users.noreply.github.com>
2021-10-07 16:42:35 +0300

    builder

--- lib/pages/blogs_widget.dart
+++ lib/pages/blogs_widget.dart
@@ -1,12 +1,8 @@
-// ignore: unused_import
-
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:tech_cuttie/pages/posts_widget.dart';
 import 'package:wordpress_api/wordpress_api.dart';
-import 'package:html/parser.dart';
-
-import 'package:tech_cuttie/api/wp_api.dart';
 
 WordPressAPI api = WordPressAPI('wp-site.com');
 
@@ -19,160 +15,65 @@ class BlogsWidget extends StatefulWidget {
 
 class _BlogsWidgetState extends State<BlogsWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('Blogs').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: scaffoldKey,
-        // appBar: AppBar(
-        //   backgroundColor: Colors.deepPurple,
-        //   automaticallyImplyLeading: true,
-        //   actions: const [],
-        //   centerTitle: true,
-        //   elevation: 4,
-        // ),
-        // backgroundColor: Color(0xFFF5F5F5),
-        body: FutureBuilder<List>(
-          future: fetchWpPosts(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Scrollbar(
-                child: ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    Map wppost = snapshot.data![index];
-                    var imageurl = wppost["featured_image_src"];
-                    var title = wppost['title']['rendered'];
-                    var link = wppost['link'];
-                    var content =
-                        parse((wppost['excerpt']['rendered']).toString())
-                            .documentElement!
-                            .text;
-                    var htmlContent = wppost['content']['rendered'];
-
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Posts(
-                                      content: content,
-                                      link: link,
-                                      imageurl: imageurl,
-                                      title: title,
-                                      htmlContent: htmlContent,
-                                    )));
-                      },
-                      child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Card(
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(40),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                children: [
-                                  FadeInImage.assetNetwork(
-                                    placeholder: 'assets/images/loading.gif',
-                                    image: imageurl,
-                                  ),
-                                  Text(wppost['title']['rendered'],
-                                      style: const TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold)),
-                                  Text(parse((wppost['excerpt']['rendered'])
-                                          .toString())
-                                      .documentElement!
-                                      .text
-                                      .replaceAll(
-                                          'Click Your preferred platform to share:PocketTelegramWhatsAppTweetPrintShare on Tumblr',
-                                          ""))
-                                ],
-                              ),
-                            ),
-                          )),
-                    );
-                  },
-                ),
-              );
-            }
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Image.asset('assets/images/loading.gif'),
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Please wait as the latest posts update...'),
-                    )
-                  ],
-                ),
-              ),
-            );
-          },
-        ));
-  }
-}
-
-class PostTile extends StatefulWidget {
-  final String title, href, desc, content;
-  // ignore: use_key_in_widget_constructors
-  const PostTile({
-    required this.content,
-    required this.desc,
-    required this.href,
-    required this.title,
-  });
-
-  @override
-  State<PostTile> createState() => _PostTileState();
-}
-
-class _PostTileState extends State<PostTile> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(40),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FutureBuilder(
-                  future: fetchWpPostImageUrl(widget.href),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Image.network(widget.href);
-                    }
-                    return Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset('assets/images/loading.gif'),
+      key: scaffoldKey,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _usersStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot doc = snapshot.data!.docs[index];
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          CachedNetworkImage(imageUrl: doc['featured_media']),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(doc['name'],
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                )),
+                          ),
+                          Text(doc['excerpt'],
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 14,
+                              )),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(doc['likes'].toString())),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(doc['views'].toString())),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(doc['comment_status'])),
+                            ],
+                          )
+                        ],
                       ),
-                    );
-                  }),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  widget.title,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(widget.content),
-              )
-            ],
-          ),
-        ),
-      ],
+                    ),
+                  );
+                });
+          } else {
+            return const Text("No data");
+          }
+        },
+      ),
     );
   }
 }
